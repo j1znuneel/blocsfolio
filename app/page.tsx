417763102e8440109ffc0c62d38b845e35c09498
j1znuneel <jishnuputhilotneelamana@gmail.com>
2025-06-27 22:10:51 +0530

    Tweakcn added

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-    <h1 className="font-mono">Hi my name is jishnu</h1>
+      <h1>Hi my name is jishnu</h1>
     </>
   );
 }
